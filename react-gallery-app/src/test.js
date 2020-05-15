de44f22892a16b3b027e0f05dba7ee4af7d74658
Grudzien Paul <paul.grudzien@zaclys.net>
2020-05-16 00:18:50 +0200

    Move files

--- react-gallery-app/src/test.js
+++
@@ -1,6 +0,0 @@
-mainTopicsPhotos = {}
-mainTopicsPhotos["Cats"] = ["kitty", "cookie"]
-mainTopicsPhotos["Dogs"] = ["largo", "havan", "idy"]
-console.log(mainTopicsPhotos)
-topic = "Cats"
-console.log(mainTopicsPhotos[topic])
